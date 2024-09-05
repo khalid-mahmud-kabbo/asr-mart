a1155631cc8825dcff7d135e91b886b566322545
Khalid Mahmud <dev.khalidmahmud01@gmail.com>
2024-09-05 22:39:27 +0600

    AAA

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'ASR Mart'), '_').'_session'
     ),
 
     /*
